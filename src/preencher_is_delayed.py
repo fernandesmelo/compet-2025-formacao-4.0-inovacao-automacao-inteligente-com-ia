1c60651799070c9fea34858ea18c1f4792ea5e99
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-24 21:35:12 -0300

    Adiciona preenchimento inicial da coluna 'Delay (min)' com zero para as primeiras linhas

--- src/preencher_is_delayed.py
+++ src/preencher_is_delayed.py
@@ -2,8 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("../data/Enhanced_pizza_sell_data_2024-25.csv", sep=',')
 
-df['Delay (min)'] = pd.to_numeric(df['Delay (min)'].astype(str).str.strip().str.replace('"', ''), errors='coerce').fillna(0)
+df.loc[:4, 'Delay (min)'] = 0
 
+df['Delay (min)'] = pd.to_numeric(df['Delay (min)'].astype(str).str.strip().str.replace('"', ''), errors='coerce').fillna(0)
 df['Is Delayed'] = df['Delay (min)'].apply(lambda x: 1 if x > 0 else 0)
 
 df.to_csv("../data/Enhanced_pizza_sell_data_2024-25.csv", index=False)
