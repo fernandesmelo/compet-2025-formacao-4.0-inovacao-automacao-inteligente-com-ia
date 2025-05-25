cfc2a1759f1c5cf21b2847a8f9821f10ff878e3e
Laércio Fernandes Correia de Melo Neto <laerciofernandesmelonetoo@gmail.com>
2025-05-24 21:33:00 -0300

    Refatora a lógica de preenchimento da coluna 'Is Delayed' para garantir a conversão correta dos valores de atraso

--- src/preencher_is_delayed.py
+++ src/preencher_is_delayed.py
@@ -2,7 +2,9 @@ import pandas as pd
 
 df = pd.read_csv("../data/Enhanced_pizza_sell_data_2024-25.csv", sep=',')
 
-df['Is Delayed'] = df['Delay (min)'].apply(lambda x: 1 if pd.to_numeric(x, errors='coerce') > 0 else 0)
+df['Delay (min)'] = pd.to_numeric(df['Delay (min)'].astype(str).str.strip().str.replace('"', ''), errors='coerce').fillna(0)
+
+df['Is Delayed'] = df['Delay (min)'].apply(lambda x: 1 if x > 0 else 0)
 
 df.to_csv("../data/Enhanced_pizza_sell_data_2024-25.csv", index=False)
 
